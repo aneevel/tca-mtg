e8373f93d4f1136d0f14640c26d051af682fee1e
Alec Neevel <aneevel15@gmail.com>
2021-02-25 20:30:12 -0600

    Create Play/Stats/Users buttons for home page

--- mtg/src/app/app.module.ts
+++ mtg/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
     BrowserAnimationsModule
   ],
   providers: [],
