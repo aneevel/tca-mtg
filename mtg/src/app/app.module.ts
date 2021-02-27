d4ec0bc30f9d3484169f1497e1d8b2af95653680
Alec Neevel <aneevel15@gmail.com>
2021-02-26 22:09:58 -0600

    Add router module

--- mtg/src/app/app.module.ts
+++ mtg/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { GameSetupComponent } from './game-setup/game-setup.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GameSetupComponent } from './game-setup/game-setup.component';
 
 @NgModule({
   declarations: [
