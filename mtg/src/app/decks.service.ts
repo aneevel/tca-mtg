60b4336cc1cd70dd08e4a16172bcfe8b094f0bef
Alec Neevel <aneevel15@gmail.com>
2021-03-22 15:33:06 -0500

    Create decks service

---
+++ mtg/src/app/decks.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+import { Deck } from './deck';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DecksService {
+
+  decks: Deck[] = [];
+
+  addDeck(deck) {
+  this.decks.push(deck);
+  }
+
+  getDecks() {
+    return this.decks;
+  }
+
+  getDeck(deck) {
+  return this.decks.find(deck);
+  }
+
+  constructor() { }
+}
