56a8d68d2c1027924e3b15f8b9441fc5b9c80f4a
Alec Neevel <aneevel15@gmail.com>
2021-04-09 15:26:57 -0500

    Add basic local storage service

---
+++ mtg/src/app/storage.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  setItem(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  getItem(key: string) {
+    return localStorage.getItem(key);
+  }
+
+  public removeItem(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public clear() {
+    localStorage.clear();
+  }
+}
