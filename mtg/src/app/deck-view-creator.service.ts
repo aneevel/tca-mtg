ba9fcf3e21dc6e0b07fa48cadb37d11ccf14b9c3
Alec Neevel <aneevel15@gmail.com>
2021-04-26 12:58:24 -0500

    Successfully generate deck views

--- mtg/src/app/deck-view-creator.service.ts
+++ mtg/src/app/deck-view-creator.service.ts
@@ -18,13 +18,60 @@ export class DeckViewCreatorService {
 
   createDeckContainer(deck: Deck): HTMLElement {
 
+    // Top level container
     const deckContainer = this.renderer.createElement('div');
+    this.renderer.setAttribute(deckContainer, 'class', 'deck-container');
+
+    // Name header
+    const deckNameHeader = this.renderer.createElement('h2');
+    this.renderer.setProperty(deckNameHeader, 'innerHTML', deck.name);
+
+    // Mana container
+    const manaContainer = this.createManaContainer(deck.colors);
+    
+    // Description container
+    const descriptionContainer = this.createDescriptionContainer(deck.description);
+
+    // Tie all HTML elements together
+    this.renderer.appendChild(deckContainer, deckNameHeader);
+    this.renderer.appendChild(deckContainer, manaContainer);
+    this.renderer.appendChild(deckContainer, descriptionContainer);
+
     return deckContainer;
+  }
+
+  createManaContainer(colors: string[]): HTMLElement {
+    const manaContainer = this.renderer.createElement('div');
+
+    // Create a mana symbol for each color passed in
+    let manaSymbols: HTMLElement[] = colors.map(color => this.createManaSymbol(color));
+
+    // Append symbols
+    manaSymbols.forEach(symbol => this.renderer.appendChild(manaContainer, symbol));
+
+    return manaContainer;
+  }
+
+  createDescriptionContainer(description: string): HTMLElement {
+    const descriptionContainer = this.renderer.createElement('div');
+    
+    const descriptionText = this.renderer.createElement('p');
+    this.renderer.setProperty(descriptionText, 'innerHTML', description);
+
+    this.renderer.appendChild(descriptionContainer, descriptionText);
+
+    return descriptionContainer;
+  }
+
+  createManaSymbol(color: string): HTMLElement {
+    const manaSymbol = this.renderer.createElement('img');
+
+    return manaSymbol;
   }
 
   generateDeckViews(decks: Deck[]) {
     const deckViews = [];
-    deckViews.forEach(deck => deckViews.push(this.createDeckContainer(deck)));
+    decks.forEach(deck => deckViews.push(this.createDeckContainer(deck)));
 
     return deckViews;
   }
