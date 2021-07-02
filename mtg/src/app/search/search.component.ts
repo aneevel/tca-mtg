96c8bb8dbc64fc33b7a145bf97ae3dbee7a5bea5
aneevel <aneevel15@gmail.com>
2021-07-02 17:55:46 -0500

    Add ng-container and getter for colors

--- mtg/src/app/search/search.component.ts
+++ mtg/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -6,14 +6,36 @@ import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
   searchForm: FormGroup;
 
-  
+  ngOnInit() {
+    this.searchForm = new FormGroup({
+      timesUsed: new FormControl(''),
+      timesWon: new FormControl(''),
+      timesLost: new FormControl(''),
+      colors: new FormArray([
+        new FormControl('Red'),
+        new FormControl('Blue'),
+        new FormControl('Green'),
+        new FormControl('Black'),
+        new FormControl('White')
+      ]),
+      winningPercentage: new FormControl('')
+    })
+  }
 
-  onSearch() {
-    console.log("Searching for values");
+  onSearch(form: FormGroup) {
+      console.log("Searching with values...");
+      console.log("Times Used:", form.value.timesUsed);
+      console.log("Times Won:", form.value.timesWon);
+      console.log("Times Lost:", form.value.timesLost);
+      console.log("Colors Included:", form.value.colorsSelection);
+  }
+
+  get colors() {
+    return this.searchForm.get('colors') as FormArray;
   }
 
 }
